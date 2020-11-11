e473338399ccab5f8d1321679e2098dfef6ad2ea
Steven Turturo <steven@avocagrow.com>
2020-11-11 04:52:16 -0700

    Created some new classes

--- Models/Character.cs
+++ Models/Character.cs
@@ -2,6 +2,12 @@ namespace dotnet_rpg.Models
 {
     public class Character
     {
-        
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public int HitPoints { get;set;}
+        public int Strength {get;set;}
+        public int Defense {get;set;}
+        public int Intelligence {get;set;}
+        public RpgClass Class {get;set;} = RpgClass.Knight;
     }
 }
